a9798ed18c2d291b28d82a8abe96a4f1b0794131
saulholding <saulholding8@gmail.com>
2021-05-10 17:20:31 -0400

    minor update

--- elephas_libs.sh
+++ elephas_libs.sh
@@ -2,4 +2,4 @@
 
 sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install -Iv pyspark==2.4.7
-sudo python3 -m pip install elephas sklearn pandas
+sudo python3 -m pip install elephas sklearn pandas boto3 s3fs
